0bbcecf4312282a2b11272608559fce85f284a05
root <ccha97u@gmail.com>
2016-07-15 08:41:39 +0000

    Enable multple_caltech_training takes GPU ID

--- experiments/scripts/multiple_caltech_test.py
+++ experiments/scripts/multiple_caltech_test.py
@@ -2,14 +2,15 @@ import sys
 from subprocess import call
 import glob
 
-version = sys.argv[1]
+GPU_ID = sys.argv[1]
+version = sys.argv[2]
 
 version_to_path = {"person_class":"person_class_only","all":"all_training_data","reasonable":"reasonable"}
 
 
 model_path = "/root/data/CalTech_models/{}/".format(version_to_path[version])
 models = glob.glob(model_path+ "*.caffemodel")
-GPU_ID = "3"
+#GPU_ID = "3"
 net = "VGG16"
 imdb = "caltech_{}".format(version)
 script_path = "experiments/scripts/caltech_test_only.sh"
